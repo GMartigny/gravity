ec3051ac1209f2dc1359dc0140393881d51d27fe
Guillaume Martigny <guillaume.martigny@gmail.com>
2019-05-14 17:56:11 +0200

    :recycle: Refactoring code. Generate html using webpack plugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+import "./style.css";
 import loader from "./loader";
 import { prepareScreens, displayScreen } from "./screens";
+import { version, author, homepage, bugs } from "../package";
 
-const prepare = async () => prepareScreens(
-    document.getElementById("scene"),
-    await loader(),
-);
+const prepare = async () => {
+    const canvas = document.createElement("canvas");
+    canvas.width = 1200;
+    canvas.height = 900;
+    document.body.appendChild(canvas);
+    return prepareScreens(
+        canvas,
+        await loader(),
+    );
+};
 
 (async () => {
+    console.log(`Gravity v${version} made with love by ${author}`);
+    console.log(`Hi, wanna get technical ? Go to ${homepage}.`);
+    console.log(`If you want to report a bug or request a feature, you can do so at ${bugs.url}`);
     const screens = await prepare();
     displayScreen(screens.title);
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
-const webpack = require("webpack");
-const { resolve } = require("path");
+const { DefinePlugin } = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env, argv) => ({
     entry: "./src/index.js",
-    output: {
-        path: resolve("./"),
-    },
     plugins: [
-        new webpack.DefinePlugin({
+        new HtmlWebpackPlugin({
+            title: "Gravity",
+        }),
+        new DefinePlugin({
             EDITOR: argv.mode === "development",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 });
